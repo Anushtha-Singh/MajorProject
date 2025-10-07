cf83f267a9efed67c6463263e70819ff0e46e124
Moha Manjhi <amanjhi1315@gmail.com>
2025-10-07 13:25:07 +0530

    v3 started

--- yojna-saathi-frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white py-6 mt-10 text-center">
-      <p>© {new Date().getFullYear()} Yojna Saathi. All rights reserved.</p>
-    </footer>
-  );
-}
